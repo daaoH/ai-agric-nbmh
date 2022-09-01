4220455ac3fe58107265445230c79e3d8614ad42
18988287996 <11454329+ynhszn_2_0@user.noreply.gitee.com>
2022-09-01 17:15:35 +0800

    新增营销和搜索模块基础信息

--- ai-agric-marketing/src/main/java/com/hszn/nbmh/marketing/AiAgricMarketingApplication.java
+++ ai-agric-marketing/src/main/java/com/hszn/nbmh/marketing/AiAgricMarketingApplication.java
@@ -1,9 +1,19 @@
 package com.hszn.nbmh.marketing;
 
+import com.hszn.nbmh.common.annotation.EnableSwagger;
+import com.hszn.nbmh.common.feign.annotation.EnableCustomFeignClients;
+import com.hszn.nbmh.common.security.annotation.EnableResourceServer;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableSwagger
+@EnableResourceServer
+@EnableCustomFeignClients
+@MapperScan("com.hszn.nbmh.*.mapper")
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {"com.hszn.nbmh", "com.hszn.nbmh.marketing", "com.hszn.nbmh.common"})
 public class AiAgricMarketingApplication {
 
     public static void main(String[] args) {
