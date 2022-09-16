e08eff44f7bcf08b58598186448e4e6de454760b
liwei <781478854@qq.com>
2022-09-16 15:16:11 +0800

    oss完善文件图片类型判断,新增视频格式判断

--- ai-agric-third-server/src/main/java/com/hszn/nbmh/third/service/impl/OssUploadServiceImpl.java
+++ ai-agric-third-server/src/main/java/com/hszn/nbmh/third/service/impl/OssUploadServiceImpl.java
@@ -12,7 +12,9 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 @Slf4j
@@ -23,51 +25,62 @@ public class OssUploadServiceImpl implements OssUploadService {
 
     @Autowired
     public OssUploadServiceImpl(AliyunOSSUtil aliyunOSSUtil) {
-        this.aliyunOSSUtil=aliyunOSSUtil;
+        this.aliyunOSSUtil = aliyunOSSUtil;
     }
 
     @Value("${oss.filePath}")
     private String filePath;
 
+    private static final List<String> IMAGES_TYPE = List.of("png", "jpg", "jpeg", "bmp", "gif", "webp", "tiff");
+
+    private static final List<String> VIDEOS_TYPE = List.of("mp4", "avi", "flv", "mov");
+
+
     @Override
     public OssData upload(MultipartFile file) {
         // 返回客户端文件系统中的原始文件名
-        String fileName=file.getOriginalFilename();
+        String fileName = file.getOriginalFilename();
         System.out.println(fileName);
         try {
-            OssData ossData=new OssData();
+            OssData ossData = new OssData();
             if (file != null) {
 
                 ossData.setFileName(fileName);
                 // 判定文件名是否为 ""
                 if (!"".equals(fileName.trim())) {
-                    File newFile=new File(fileName);
-                    FileOutputStream os=new FileOutputStream(newFile);
+                    File newFile = new File(fileName);
+                    FileOutputStream os = new FileOutputStream(newFile);
                     // 以字节数组的形式返回文件的内容,再输出到文件输出流中
                     os.write(file.getBytes());
                     os.close();
                     // 将接受的文件传输到给定的目标文件 file-->newFile
                     file.transferTo(newFile);
                     // 根据不同文件 类型/日期 生成不同的文件夹
-                    String datePath=new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-                    String timeStamp=String.valueOf(System.currentTimeMillis());
-                    ossData.setType(fileName.substring(fileName.lastIndexOf(".") + 1));
-                    fileName=timeStamp + fileName.substring(fileName.lastIndexOf("."));
+                    String datePath = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+                    String timeStamp = String.valueOf(System.currentTimeMillis());
+                    String fileType = getFileType(fileName);
+                    ossData.setType(fileType);
+                    fileName = timeStamp + "." + fileType;
                     String path;
-
-                    if (fileName.endsWith(".jpg") || fileName.endsWith(".jpeg") || fileName.endsWith(".png")) {
-                        path=filePath + "images/" + datePath + fileName;
+                    //图片格式 png、jpg、jpeg、bmp、gif、webp、tiff
+                    if (IMAGES_TYPE.contains(fileType)) {
+                        path = filePath + "images/" + datePath + fileName;
+                        ossData.setPath(path);
+                    }
+                    //mp4、avi、flv、mov 视频格式
+                    if (VIDEOS_TYPE.contains(fileType)) {
+                        path = filePath + "videos/" + datePath + fileName;
                         ossData.setPath(path);
                     } else {
-                        path=filePath + "other/" + datePath + fileName;
+                        path = filePath + "other/" + datePath + fileName;
                         ossData.setPath(path);
                     }
                     // 上传到OSS
-                    String uploadUrl=aliyunOSSUtil.upLoad(newFile, path);
+                    String uploadUrl = aliyunOSSUtil.upLoad(newFile, path);
                     newFile.delete();
                     if (uploadUrl != null) {
                         //拼接下载路径全路径
-                        ossData.setPath("https://nbmhoss.oss-cn-shanghai.aliyuncs.com/"+ossData.getPath());
+                        ossData.setPath("https://nbmhoss.oss-cn-shanghai.aliyuncs.com/" + ossData.getPath());
                         return ossData;
                     }
                 }
@@ -76,5 +89,21 @@ public class OssUploadServiceImpl implements OssUploadService {
             e.printStackTrace();
         }
         return null;
+    }
+
+    /**
+     * 获取文件类型
+     * <p>
+     * 例如: yyx.txt, 返回: txt
+     *
+     * @param fileName 文件名
+     * @return 后缀（不含".")
+     */
+    private String getFileType(String fileName) {
+        int separatorIndex = fileName.lastIndexOf(".");
+        if (separatorIndex < 0) {
+            return "";
+        }
+        return fileName.substring(separatorIndex + 1).toLowerCase();
     }
 }
